29bfc0b12d81b15e7735d16319e84948067f4f6e
Edlyn Villegas <edlynsanantonio@gmail.com>
2020-02-08 20:02:51 -0800

    Updated files...

--- evo-calendar.js
+++ evo-calendar.js
@@ -151,16 +151,32 @@
         // compensate for leap year
         if (new_month == 1) { // February only!
             if((new_year % 4 == 0 && new_year % 100 != 0) || new_year % 400 == 0){
-            monthLength = 29;
+                monthLength = 29;
             }
         }
         // do the header
         
         var monthName =  _.$cal_months_labels[new_month];
 
-        var mainHTML = '<div class="calendar-sidebar"></div><div class="calendar-inner"></div><div class="calendar-events"></div>';
+        var mainHTML = '';
+        var sidebarHTML = '';
+        var calendarHTML = '';
+        
 
-        var sidebarHTML = '<div class="calendar-year"><img year-val="prev" title="Previous year" src="evo-calendar/chevron-left.png"/>&nbsp;<p>'+new_year+'&nbsp;</p><img year-val="next" title="Next year" src="evo-calendar/chevron-right.png"/></div>';
+        
+        function buildMainHTML() {
+            var mainHTML = '<div class="calendar-sidebar"></div><div class="calendar-inner"></div><div class="calendar-events"></div>';
+            if(_.options.canAddEvent) {
+                mainHTML += '<span id="eventAddButton" title="Add event">ADD EVENT</span>';
+            }
+            if(_.options.eventListToggler) {
+                mainHTML += '<span id="eventListToggler" title="Close event list"><button class="icon-button"><span class="chevron-arrow-right"></span></button></span>';
+            }
+            _.$mainHTML = mainHTML;
+        }
+
+        function buildSidebarHTML() {
+            sidebarHTML = '<div class="calendar-year"><button class="icon-button" year-val="prev" title="Previous year"><span class="chevron-arrow-left"></span></button>&nbsp;<p>'+new_year+'&nbsp;</p><button class="icon-button" year-val="next" title="Next year"><span class="chevron-arrow-right"></span></button></div>';
             sidebarHTML += '<ul class="calendar-months">';
             for(var i = 0; i < _.$cal_months_labels.length; i++) {
                 sidebarHTML += '<li class="month';
@@ -169,10 +185,13 @@
             }
             sidebarHTML += '</ul>';
             if(_.options.sidebarToggler) {
-                sidebarHTML += '<span id="sidebarToggler" title="Close sidebar"><img src="evo-calendar/bars.png"/></span>';
+                sidebarHTML += '<span id="sidebarToggler" title="Close sidebar"><button class="icon-button"><span class="bars"></span></button></span>';
             }
+            _.$sidebarHTML = sidebarHTML;
+        }
 
-        var calendarHTML = '<table class="calendar-table">';
+        function buildCalendarHTML() {
+            calendarHTML = '<table class="calendar-table">';
             calendarHTML += '<tr><th colspan="7">';
             calendarHTML +=  _.formatDate(new Date(monthName +' '+ new_year), _.options.titleFormat, 'en');
             calendarHTML += '</th></tr>';
@@ -182,66 +201,79 @@
                 calendarHTML += _.$cal_days_labels[i];
                 calendarHTML += '</td>';
             }
-        calendarHTML += '</tr><tr class="calendar-body">';
-
-
-
-        // fill in the days
-        var day = 1;
-        // this loop is for is weeks (rows)
-        for (var i = 0; i < 9; i++) {
-            // this loop is for weekdays (cells)
-            for (var j = 0; j <= 6; j++) { 
-                calendarHTML += '<td class="calendar-day">';
-                if (day <= monthLength && (i > 0 || j >= startingDay)) {
-                    var thisDay = _.formatDate(new Date(monthName +'/'+ day +'/'+ new_year), _.options.format, 'en');
-                    calendarHTML += '<div class="day'
-                    calendarHTML += ((_.$active_date === thisDay) ? ' calendar-active' : '') + '" date-val="'+thisDay+'">'+day+'</div>';
-                    day++;
+            calendarHTML += '</tr><tr class="calendar-body">';
+            // fill in the days
+            var day = 1;
+            // this loop is for is weeks (rows)
+            for (var i = 0; i < 9; i++) {
+                // this loop is for weekdays (cells)
+                for (var j = 0; j <= 6; j++) { 
+                    calendarHTML += '<td class="calendar-day">';
+                    if (day <= monthLength && (i > 0 || j >= startingDay)) {
+                        var thisDay = _.formatDate(new Date(monthName +'/'+ day +'/'+ new_year), _.options.format, 'en');
+                        calendarHTML += '<div class="day'
+                        calendarHTML += ((_.$active_date === thisDay) ? ' calendar-active' : '') + '" date-val="'+thisDay+'">'+day+'</div>';
+                        day++;
+                    }
+                    calendarHTML += '</td>';
                 }
-                calendarHTML += '</td>';
-            }
-            // stop making rows if we've run out of days
-            if (day > monthLength) {
-              break;
-            } else {
-              calendarHTML += '</tr><tr class="calendar-body">';
-            }
-        }
-        calendarHTML += '</tr></table>';
-
-        // var eventHTML = '<div class="calendar-events-header"><p>'+_.$active_month+' '+_.$active_date+', '+_.$active_year+'</p></div>';
-        if(_.options.calendarEvents != null) {
-            var eventHTML = '<div class="event-header"><p>'+_.formatDate(new Date(_.$active_date), _.options.eventHeaderFormat, 'en')+'</p></div>';
-            var hasEventToday = false;
-            console.log(_.$active_date)
-            eventHTML += '<div>';
-            for (var i = 0; i < _.options.calendarEvents.length; i++) {
-                if(_.$active_date === _.options.calendarEvents[i].date) {
-                    hasEventToday = true;
-                    eventHTML += '<div class="event-container">';
-                    eventHTML += '<div class="event-icon"><div class="event-bullet-'+_.options.calendarEvents[i].type+'"></div></div>';
-                    eventHTML += '<div class="event-info"><p>'+_.options.calendarEvents[i].name+'</p></div>';
-                    eventHTML += '</div>';
+                // stop making rows if we've run out of days
+                if (day > monthLength) {
+                  break;
+                } else {
+                  calendarHTML += '</tr><tr class="calendar-body">';
                 }
-            };
-            if(!hasEventToday) {
-                eventHTML += '<p>No event for this day.. so take a rest! :)</p>';
             }
-            eventHTML += '</div>';
-            _.$eventHTML = eventHTML;
+            calendarHTML += '</tr></table>';
+            _.$calendarHTML = calendarHTML;
+        }
+        
+        function buildEventListHTML() {
+            console.log('buildEventListHTML()')
+            if(_.options.calendarEvents != null) {
+                var eventHTML = '<div class="event-header"><p>'+_.formatDate(new Date(_.$active_date), _.options.eventHeaderFormat, 'en')+'</p></div>';
+                var hasEventToday = false;
+                eventHTML += '<div>';
+                for (var i = 0; i < _.options.calendarEvents.length; i++) {
+                    if(_.$active_date === _.options.calendarEvents[i].date) {
+                        hasEventToday = true;
+                        eventHTML += '<div class="event-container">';
+                        eventHTML += '<div class="event-icon"><div class="event-bullet-'+_.options.calendarEvents[i].type+'"></div></div>';
+                        eventHTML += '<div class="event-info"><p>'+_.options.calendarEvents[i].name+'</p></div>';
+                        eventHTML += '</div>';
+                    } else if (_.options.calendarEvents[i].everyYear) {
+                        var d = _.formatDate(new Date(_.$active_date), 'mm/dd', 'en');
+                        var dd = _.formatDate(new Date(_.options.calendarEvents[i].date), 'mm/dd', 'en');
+                        if(d==dd) {
+                            hasEventToday = true;
+                            eventHTML += '<div class="event-container">';
+                            eventHTML += '<div class="event-icon"><div class="event-bullet-'+_.options.calendarEvents[i].type+'"></div></div>';
+                            eventHTML += '<div class="event-info"><p>'+_.options.calendarEvents[i].name+'</p></div>';
+                            eventHTML += '</div>';
+                        }
+                    }
+                };
+                if(!hasEventToday) {
+                    eventHTML += '<p>No event for this day.. so take a rest! :)</p>';
+                }
+                eventHTML += '</div>';
+                _.$eventHTML = eventHTML;
+            }
         }
 
-        if(_.options.canAddEvent) {
-            mainHTML += '<span id="eventAddButton" title="Add event">ADD EVENT</span>';
+        if(val == 'all') {
+            buildMainHTML();
+            buildSidebarHTML();
+            buildCalendarHTML();
+            buildEventListHTML();
+        } else if (val == 'sidebar') {
+            buildSidebarHTML();
+        } else if (val == 'inner') {
+            console.log('buildCalendar---inner', _.options.calendarEvents);
+            buildCalendarHTML();
+        } else if (val == 'events') {
+            buildEventListHTML();
         }
-        if(_.options.eventListToggler) {
-            mainHTML += '<span id="eventListToggler" title="Close event list"><img src="evo-calendar/chevron-right.png"/></span>';
-        }
-
-        _.$mainHTML = mainHTML;
-        _.$sidebarHTML = sidebarHTML;
-        _.$calendarHTML = calendarHTML;
 
         _.setHTML(val);
     };
@@ -286,24 +318,48 @@
         var _ = this;
         // prevent duplication
         $('.event-indicator').empty();
+        // find number of days in month
+        var monthLength = _.$cal_days_in_month[_.$active_month];
+
+        // compensate for leap year
+        if (_.$active_month == 1) { // February only!
+            if((_.$active_year % 4 == 0 && _.$active_year % 100 != 0) || _.$active_year % 400 == 0){
+                monthLength = 29;
+            }
+        }
+        
         for (var i = 0; i < _.options.calendarEvents.length; i++) {
-            for (var x = 0; x < _.$cal_days_in_month[_.$active_month]; x++) {
+            for (var x = 0; x < monthLength; x++) {
                 var active_date = _.formatDate(new Date(_.$cal_months_labels[_.$active_month] +'/'+ (x + 1) +'/'+ _.$active_year), _.options.format, 'en');
                 // console.log(active_date, _.formatDate(new Date(_.options.calendarEvents[i].date), _.options.format, 'en'))
+                
+                var thisDate = $('[date-val="'+active_date+'"]');
                 if(active_date==_.options.calendarEvents[i].date) {
-                    var thisDate = $('[date-val="'+active_date+'"]');
-                    $('[date-val="'+active_date+'"]').addClass('calendar-'+ _.options.calendarEvents[i].type);
+                    thisDate.addClass('calendar-'+ _.options.calendarEvents[i].type);
 
                     if($('[date-val="'+active_date+'"] span.event-indicator').length == 0) {
-                        $('[date-val="'+active_date+'"]').append('<span class="event-indicator"></span>');
+                        thisDate.append('<span class="event-indicator"></span>');
                     }
 
                     if($('[date-val="'+active_date+'"] span.event-indicator > .type-bullet > .type-'+_.options.calendarEvents[i].type).length == 0) {
                         var htmlToAppend = '<div class="type-bullet"><div class="type-'+_.options.calendarEvents[i].type+'"></div></div>';
-                        $('[date-val="'+active_date+'"]').find('.event-indicator').append(htmlToAppend);
+                        thisDate.find('.event-indicator').append(htmlToAppend);
                     }
-
-
+                } else if (_.options.calendarEvents[i].everyYear) {
+                    var d = _.formatDate(new Date(active_date), 'mm/dd', 'en');
+                    var dd = _.formatDate(new Date(_.options.calendarEvents[i].date), 'mm/dd', 'en');
+                    if(d==dd) {
+                        thisDate.addClass('calendar-'+ _.options.calendarEvents[i].type);
+    
+                        if($('[date-val="'+active_date+'"] span.event-indicator').length == 0) {
+                            thisDate.append('<span class="event-indicator"></span>');
+                        }
+    
+                        if($('[date-val="'+active_date+'"] span.event-indicator > .type-bullet > .type-'+_.options.calendarEvents[i].type).length == 0) {
+                            var htmlToAppend = '<div class="type-bullet"><div class="type-'+_.options.calendarEvents[i].type+'"></div></div>';
+                            thisDate.find('.event-indicator').append(htmlToAppend);
+                        }
+                    }
                 }
             }
         };
@@ -326,9 +382,8 @@
         if(_.options.canAddEvent) {
             $('#eventAddButton')
                .off('click.evocalendar')
-               .on('click.evocalendar', _.addCalendarEvent);
+               .on('click.evocalendar', _.options.onAddEvent);
         }
-
 
 
         $('[date-val]')
@@ -415,19 +470,6 @@
         }
     };
 
-    // toggle event list
-    EvoCalendar.prototype.addCalendarEvent = function(new_data) {
-        var _ = this;
-        var data = new_data;
-        for(var i=0; i < data.length; i++) {
-            if(_.isValidDate(data[i].date)) {
-                data[i].date = _.formatDate(new Date(data[i].date), _.options.format, 'en');
-                _.options.calendarEvents.push(data[i]);
-            }
-        }
-         _.buildCalendar('inner');
-         _.buildCalendar('events');
-    };
     // toggle event list
     EvoCalendar.prototype.addCalendarEvent = function(new_data) {
         var _ = this;
